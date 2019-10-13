9a230d19c81a0678787ff64ab6a6df82553dbb99
n4beel1 <na268666@gmail.com>
2019-10-13 20:54:09 +0500

    working

--- scrape.js
+++ scrape.js
@@ -10,84 +10,98 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-for (let i = 17001; i <= 17100; i++) {
+const getInfo = inputUrl => {
+    return new Promise((resolve, reject) => {
+        request({
+            "rejectUnauthorized": false,
+            "url": `https://events.hec.gov.pk/portal_dashboard/view_profile.php?id=${inputUrl}`,
+            "method": "GET",
+            "headers": {
+                "X-API-VERSION": 1
+            }
+        }, (error, respone, html) => {
+            if (!error && respone.statusCode == 200) {
+                // console.log(html);
+                let information = {};
+                const $ = cheerio.load(html);
+                information.id = $('#emp_id') == null ? "" : $('#emp_id').val();
+                information.name = $('#sname') == null ? "" : $('#sname').val();
+                information.surName = $('#father_name') == null ? "" : $('#father_name').val();
+                information.gender = $('#gender') == null ? "" : $('#gender').val();
+                information.cnic = $('#cnic') == null ? "" : $('#cnic').val();
+                information.dob = $('#dob') == null ? "" : $('#dob').val();
+                information.permAddress = $('#per_addres') == null ? "" : $('#per_addres').val();
+                information.resAddress = $('#pre_addres') == null ? "" : $('#pre_addres').val();
+                information.city = $('#pre_city') == null ? "" : $('#pre_city').val();
+                information.phone = $('#phone') == null ? "" : $('#phone').val();
+                information.province = $('#pre_province').children("[selected='selected']") == null ? "" : $('#pre_province').children("[selected='selected']").text();
+                information.domProvince = $('#domicil').children("[selected='selected']") == null ? "" : $('#domicil').children("[selected='selected']").text();
+                information.mail = $("div:contains('Email:')").next() == null ? "" : $("div:contains('Email:')").next().text().trim();
+
+                if (information.mail != "") {
+                    resolve(information);
+                }
+                else {
+                    reject(`Error: no contact info for id: ${inputUrl}`);
+                }
+
+            }
+            else {
+                reject("Error: Connection Timed Out" + inputUrl);
+            }
+        });
+    });
+}
+
+const getEdu = inputUrl => {
+    return new Promise((resolve, reject) => {
+        request({
+            "rejectUnauthorized": false,
+            "url": `https://events.hec.gov.pk/portal_dashboard/education.php?id=${inputUrl}`,
+            "method": "GET",
+            "headers": {
+                "X-API-VERSION": 1
+            }
+        }, (error, respone, html) => {
+            if (!error && respone.statusCode == 200) {
+                const $ = cheerio.load(html);
+                let edu = {};
+                edu.passingYear = $('#myTable').children('tr').children('td:nth-of-type(1)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(1)').text();
+                edu.qualification = $('#myTable').children('tr').children('td:nth-of-type(2)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(2)').text();
+                edu.programTitle = $('#myTable').children('tr').children('td:nth-of-type(3)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(3)').text();
+                edu.discipline = $('#myTable').children('tr').children('td:nth-of-type(4)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(4)').text();
+                edu.majorSubj = $('#myTable').children('tr').children('td:nth-of-type(5)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(5)').text();
+                edu.uniBoard = $('#myTable').children('tr').children('td:nth-of-type(6)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(6)').text();
+
+                if (true)
+                    resolve(edu);
+                else
+                    reject("Error: impossibl    e happened")
+            }
+            else {
+                reject("Error: Connection Timed Out" + inputUrl);
+            }
+        });
+
+    });
+}
+
+// const set
+
+for (let i = 12301; i <= 12400; i++) {
     userData = {
         'info': {},
         'education': {}
     };
 
-    request({
-        "rejectUnauthorized": false,
-        "url": `https://events.hec.gov.pk/portal_dashboard/view_profile.php?id=${i}`,
-        "method": "GET",
-        "headers": {
-            "X-API-VERSION": 1
-        }
-    }, async (error, respone, html) => {
-        if (!error && respone.statusCode == 200) {
-            // console.log(html);
-            const $ = cheerio.load(html);
-            userData.info.id = $('#emp_id') == null ? "" : $('#emp_id').val();
-            userData.info.name = $('#sname') == null ? "" : $('#sname').val();
-            userData.info.surName = $('#father_name') == null ? "" : $('#father_name').val();
-            userData.info.gender = $('#gender') == null ? "" : $('#gender').val();
-            userData.info.cnic = $('#cnic') == null ? "" : $('#cnic').val();
-            userData.info.dob = $('#dob') == null ? "" : $('#dob').val();
-            userData.info.permAddress = $('#per_addres') == null ? "" : $('#per_addres').val();
-            userData.info.resAddress = $('#pre_addres') == null ? "" : $('#pre_addres').val();
-            userData.info.city = $('#pre_city') == null ? "" : $('#pre_city').val();
-            userData.info.phone = $('#phone') == null ? "" : $('#phone').val();
-            userData.info.province = $('#pre_province').children("[selected='selected']") == null ? "" : $('#pre_province').children("[selected='selected']").text();
-            userData.info.domProvince = $('#domicil').children("[selected='selected']") == null ? "" : $('#domicil').children("[selected='selected']").text();
-            userData.info.mail = $("div:contains('Email:')").next() == null ? "" : $("div:contains('Email:')").next().text().trim();
+    const infoResolve = getInfo(i);
+    const eduResolve = getEdu(i);
 
-            if (userData.info.mail != "") {
-
-                // db.collection('test').doc(i + "").collection('data').doc('info').set(userData.info);
-
-
-                request({
-                    "rejectUnauthorized": false,
-                    "url": `https://events.hec.gov.pk/portal_dashboard/education.php?id=${i}`,
-                    "method": "GET",
-                    "headers": {
-                        "X-API-VERSION": 1
-                    }
-                }, async (error, respone, html) => {
-                    if (!error && respone.statusCode == 200) {
-                        const $ = cheerio.load(html);
-                        userData.education.passingYear = $('#myTable').children('tr').children('td:nth-of-type(1)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(1)').text();
-                        userData.education.qualification = $('#myTable').children('tr').children('td:nth-of-type(2)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(2)').text();
-                        userData.education.programTitle = $('#myTable').children('tr').children('td:nth-of-type(3)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(3)').text();
-                        userData.education.discipline = $('#myTable').children('tr').children('td:nth-of-type(4)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(4)').text();
-                        userData.education.majorSubj = $('#myTable').children('tr').children('td:nth-of-type(5)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(5)').text();
-                        userData.education.uniBoard = $('#myTable').children('tr').children('td:nth-of-type(6)') == null ? "" : $('#myTable').children('tr').children('td:nth-of-type(6)').text();
-
-
-                        await db.collection('test').doc(i + "").set(userData);
-                        // console.log(`data entered for ${i}`)
-
-
-                    }
-                    else {
-                        console.log(error, respone);
-                    }
-                });
-
-
-            }
-
-        }
-        else {
-            console.log(error, respone);
-        }
-    });
+    Promise.all([infoResolve, eduResolve]).then(values => {
+        userData.info = values[0];
+        userData.education = values[1];
+        db.collection('test').doc(i + "").set(userData);
+    }).catch(err => console.log(err));
 
 }
 
-
-
-
-// }
-
-// console.log(info.sort());
